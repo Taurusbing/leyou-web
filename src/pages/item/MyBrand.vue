87513c7da0c15a242edf7019320472116f96d395
Taurusbing <760494282@qq.com>
2019-03-27 23:46:24 +0800

    修改brand,完成前端数据请求

--- src/pages/item/MyBrand.vue
+++ src/pages/item/MyBrand.vue
@@ -5,6 +5,7 @@
         <v-btn color="primary">新增</v-btn>
         <v-spacer></v-spacer>
         <v-flex xs12 sm6 md3>
+          <!--搜索按钮，绑定参数-->
           <v-text-field label="搜索" append-icon="search" hide-details v-model="search"></v-text-field>
         </v-flex>
       </v-layout>
@@ -14,7 +15,7 @@
         :options.sync="options"
         :server-items-length="totalDesserts"
         :loading="loading"
-        :pagination="pagination"
+        :pagination.sync="pagination"
       >
         <!--class="elevation-1"    样式-->
         <!-- 数据渲染 -->
@@ -43,7 +44,8 @@
             branks: [],
             loading: false,
             options: {},
-            pagination:{},
+            pagination:{},//分页对象
+            search:"",
             headers: [
               { text: '品牌Id',align: 'center',sortable: true,value: 'id'},
               { text: '品牌名',align: 'center',sortable: false,value: 'name'},
@@ -53,52 +55,59 @@
             ]
           }
         },
-      mounted(){//渲染后执行
-        // this.getdata();
+      //监听
+      watch:{
+        //监听只要搜索框修改就调用函数
+        search(){
+          this.loadData()
+        },
+        pagination:{
+          //复杂参数，深度监听
+          deep:true,
+          handler(){
+            this.loadData();
+          }
+        }
+        // search:{ 另一种写法
+        //   handler(){
+        //     this.loadData()
+        //   }
+        // }
       },
+
       //钩子函数，页面一加载就创建
       created(){
-        //   this.branks = [
-        //   {id:"1234",name:"小米",image:"//",letter:"X"},
-        //   {id:"1235",name:"华为",image:"//",letter:"H"},
-        //   {id:"1134",name:"三星",image:"//",letter:"S"},
-        //   {id:"1034",name:"苹果",image:"//",letter:"P"}
-        // ];
-        //   this.totalDesserts = 15
-        //vue的ajax框架axios，then是成功的回调，catch是失败的回调
-/*        axios.get("/item/queryBrank")
-          .then(function (response) {
-            this.branks = response;
-        }).catch(function (error) {
-
-        })*/
-
-          this.$http.get("item/brand/queryAll")
-            .then(resp =>{
-              this.branks = resp.data;
-          })
-              // this.$http.get("item/brand/queryAll")
-              //   .then(function (response) {
-              //       this.branks = response.data;
-              //   }).catch(function (error) {
-              //
-              // })
-
-        }
-      // methods:{
-      //     getdata(){
-      //       this.$http.get("item/brand/queryAll")
-      //         .then(function (response) {
-      //           console.log(response.data),
-      //             this.branks = response.data
-      //         }).catch(function (error) {
-      //
-      //       })
-      //     }
-      // }
+          this.loadData()
+        },
+      methods:{
+          //定义函数
+          loadData(){
+              this.$http.get("item/brand/queryAll",{
+                params:{
+                  search:this.search,
+                  descending:this.pagination.descending,//排序方式，false是asc升序,true是desc降序
+                  page:this.pagination.page,//当前页码
+                  rows:this.pagination.rowsPerPage,//每页多少行
+                  sortBy:this.pagination.sortBy,//排序字段
+                  totalItems:this.pagination.totalItems//数据总数
+                }
+              }).then(resp =>{
+                  this.branks = resp.data
+              })
+          }
+      }
       }
 </script>
 
 <style scoped>
 
 </style>
+
+//搜索条件查询
+  //绑定事件，定义变量
+  //请求
+  //监听
+//分页查询
+  //对象操作
+  //请求参数
+  //监听
